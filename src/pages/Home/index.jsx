595d014079cf030593b505b203ba26184a06ec28
andy silva <andssilva.23@gmail.com>
2021-09-01 19:41:36 -0400

    feat: :rocket: adiciona ícones/ cria a estrutura do mapa

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,25 +1,27 @@
 import React, { useState } from 'react';
 import TextField, { Input } from '@material/react-text-field';
-import { Container, Search } from './style';
+import MaterialIcon from '@material/react-material-icon';
+import { Container, Search, Logo, Wrapper, Map } from './style';
 import logo from '../../assets/logo.svg';
 
 function Home() {
   const [inputValue, setInputValue] = useState();
 
   return (
-    <Container>
-      <Search>
-        <img src={logo} alt="restaurant search logo" />
-        <TextField
-          label="Pesquisar"
-          outlined
-          // onTrailingIconSelect={() => this.setState({value: ''})}
-          // trailingIcon={<MaterialIcon role="button" icon="delete"/>}
-        >
-          <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-        </TextField>
-      </Search>
-    </Container>
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={logo} alt="restaurant search logo" />
+          <TextField
+            label="Pesquisar restaurante"
+            outlined
+            trailingIcon={<MaterialIcon role="button" icon="search" />}>
+            <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+          </TextField>
+        </Search>
+      </Container>
+      <Map />
+    </Wrapper>
   );
 }
 
