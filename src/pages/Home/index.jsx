8ca9fd5fd59402ccb104150aaaad77a18e3f640a
andy silva <andssilva.23@gmail.com>
2021-09-01 10:33:06 -0400

    feat: :rocket: adiciona o campo de pesquisa

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
-import { Container } from './style';
+import React, { useState } from 'react';
+import TextField, { Input } from '@material/react-text-field';
+import { Container, Search } from './style';
+import logo from '../../assets/logo.svg';
 
 function Home() {
-  return <Container>Hello World!</Container>;
+  const [inputValue, setInputValue] = useState();
+
+  return (
+    <Container>
+      <Search>
+        <img src={logo} alt="restaurant search logo" />
+        <TextField
+          label="Pesquisar"
+          outlined
+          // onTrailingIconSelect={() => this.setState({value: ''})}
+          // trailingIcon={<MaterialIcon role="button" icon="delete"/>}
+        >
+          <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+        </TextField>
+      </Search>
+    </Container>
+  );
 }
 
 export default Home;
