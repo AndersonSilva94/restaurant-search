885767cc92a667e8ad682144bd6fdf31e3751370
andy silva <andssilva.23@gmail.com>
2021-09-01 20:07:54 -0400

    feat: :rocket: inicia carousel de restaurantes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,23 @@
 import React, { useState } from 'react';
 import TextField, { Input } from '@material/react-text-field';
 import MaterialIcon from '@material/react-material-icon';
+import Slider from 'react-slick';
+
 import { Container, Search, Logo, Wrapper, Map, CarouselTitle } from './style';
 import logo from '../../assets/logo.svg';
+import restaurante from '../../assets/restaurante-fake.png';
 
 function Home() {
   const [inputValue, setInputValue] = useState();
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    adaptiveHeight: true,
+  };
 
   return (
     <Wrapper>
@@ -20,6 +32,23 @@ function Home() {
           </TextField>
         </Search>
         <CarouselTitle>Na sua área</CarouselTitle>
+        <Slider {...settings}>
+          <div>
+            <img src={restaurante} alt="" />
+          </div>
+          <div>
+            <img src={restaurante} alt="" />
+          </div>
+          <div>
+            <img src={restaurante} alt="" />
+          </div>
+          <div>
+            <img src={restaurante} alt="" />
+          </div>
+          <div>
+            <img src={restaurante} alt="" />
+          </div>
+        </Slider>
       </Container>
       <Map />
     </Wrapper>
