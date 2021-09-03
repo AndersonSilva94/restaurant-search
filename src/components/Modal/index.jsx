2e77025a1fe71736caa91028de7ac3b45e29b1ff
andy silva <andssilva.23@gmail.com>
2021-09-03 09:15:02 -0400

    feat: :rocket: cria a lógica para fechar a modal ao apertar ESC

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Portal from './Portal';
 import { Overlay, Dialog } from './style';
 
 function Modal({ children, open, onClose }) {
+  useEffect(() => {
+    function onEsc(event) {
+      if (event.keyCode === 27) onClose();
+    }
+
+    window.addEventListener('keydown', onEsc);
+
+    return () => {
+      window.removeEventListener('keydown', onEsc);
+    };
+  }, [onClose]);
+
   if (!open) return null;
 
   const onOverlayClick = () => {
